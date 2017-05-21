3ce9745089b2ba490bb6c3bdbff79602a1f8e9ed
Antônio Maurício Sousa Pinheiro <toinho99@gmail.com>
2017-05-21 00:56:07 -0300

    Add files via upload

---
+++ objects/pokemon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="pokemon" tilewidth="16" tileheight="16" tilecount="2912" columns="52">
+ <image source="pokemon.png" width="832" height="896"/>
+</tileset>
