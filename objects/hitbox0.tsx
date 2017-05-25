01c9f4f1233106ff553d7e41be71abfdec0e80b0
Pedro Augusto Araujo Alves <pedroaa57@gmail.com>
2017-05-25 00:04:17 -0300

    Added Animation Files

---
+++ objects/hitbox0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="hitbox 0 " tilewidth="1" tileheight="16" tilecount="3" columns="3">
+ <image source="hitbox0.png" width="3" height="16"/>
+</tileset>
---
+++ objects/link.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="link" tilewidth="16" tileheight="16" tilecount="648" columns="27">
+ <image source="zelda.png" width="442" height="390"/>
+</tileset>
